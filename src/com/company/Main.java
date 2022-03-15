8351fe046d3111ccf96e88c45d19cb38907727bb
student <cureit321@wp.pl>
2022-03-15 17:02:56 +0100

    changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Second banch");
-        System.out.println("another line");
+        System.out.println("another lined");
 
     }
 }
