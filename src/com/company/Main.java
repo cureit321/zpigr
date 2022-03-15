df2a27b3e38553df6f46e7c632d4c219ea6e6fd6
student <cureit321@wp.pl>
2022-03-15 17:02:43 +0100

    add main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write
+        System.out.println("Second banch");
+        System.out.println("another line");
 
     }
 }
